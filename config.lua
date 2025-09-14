2c68f71df61c3dc946c237759ff72a7e6afdafae
cmd <123102218+cmdscripts@users.noreply.github.com>
2025-09-14 20:18:08 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,14 +1,28 @@
 Config = {
+    UseOXTarget = true,
+    Debug = false,
+    CooldownTime = 300000,
+    
     Position = {
         {
             position = vector3(7.1894, -932.7761, 29.9050),
             job = "police",
-            helpNotification = "Test",
+            helpNotification = "Jemand braucht Hilfe bei der Polizeistation!",
         },
         {
             position = vector3(440.29974365234, -984.40850830078, 30.724325180054),
             job = "police",
-            helpNotification = "ABC",
+            helpNotification = "Jemand klingelt am Mission Row PD!",
+        },
+        {
+            position = vector3(295.0, -1448.0, 29.0),
+            job = "ambulance",
+            helpNotification = "Jemand braucht medizinische Hilfe!",
+        },
+        {
+            position = vector3(-1037.0, -2737.0, 20.0),
+            job = "mechanic",
+            helpNotification = "Jemand braucht einen Mechaniker!",
         },
     },
 }
