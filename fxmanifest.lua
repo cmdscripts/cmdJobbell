88d5828a77ed5c17162aab2b2bc10204d2430787
cmd <123102218+cmdscripts@users.noreply.github.com>
2025-09-14 20:18:35 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,8 +1,31 @@
-fx_version 'adamant'
-games {'gta5'}
-author 'cmdscripts'
-version '1.6'
+fx_version 'cerulean'
+game 'gta5'
 
-shared_scripts { "config.lua" , '@es_extended/imports.lua'}
-client_script { "client.lua", }
-server_scripts { "@mysql-async/lib/MySQL.lua", "server.lua", }
+author 'cmdscripts'
+version '2.0'
+description 'Job Bell System with OX Target and Marker support'
+
+lua54 'yes'
+
+shared_scripts {
+    '@es_extended/imports.lua',
+    '@ox_lib/init.lua',
+    'config.lua'
+}
+
+client_scripts {
+    'client.lua'
+}
+
+server_scripts {
+    'server.lua'
+}
+
+dependencies {
+    'es_extended',
+    'ox_lib'
+}
+
+optional_dependencies {
+    'ox_target'
+}
